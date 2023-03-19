a13e1297c3cb5ad46916a2b3299e28c56bfbcd18
Simon Shamoon <simonshm5@gmail.com>
2023-03-19 15:40:02 -0500

    testing

--- src/interfaces/IUniswapV3MintCallback.sol
+++ src/interfaces/IUniswapV3MintCallback.sol
@@ -1,5 +1,7 @@
-pragma solidity ^0.8.14; 
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.14;
 
 interface IUniswapV3MintCallback {
     function uniswapV3MintCallback(uint256 amount0, uint256 amount1, bytes calldata data) external;
+    
 }
